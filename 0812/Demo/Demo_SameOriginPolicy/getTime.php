feb0219dfddf9c9c8a69edd4dcfb96ae35d73ae4
Esther-mac <esther_lin@mail.chungyo.net>
2020-08-12 16:01:14 +0800

    delete 0812floder

--- 0812/Demo/Demo_SameOriginPolicy/getTime.php
+++
@@ -1,6 +0,0 @@
-<?php
-// header("Access-Control-Allow-Origin: *");
-
-$curDate = date('Y-m-d H:i:s');
-echo '{"time": "' . $curDate . '"}';
-?>
--- 0812/Demo/VueJS/README.md
+++
@@ -1,5 +0,0 @@
-# Vue.js 前端套件應用實務
-- JavaScript
-- Vue.js 前端套件
-- AJAX
-- 與後台 Web API 整合實例
